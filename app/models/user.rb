582abca8d160cd385272a226daf2cc24821a264f
hayashihiroaki <61njn0@gmail.com>
2020-09-29 13:49:59 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/} 
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/} 
+  validates :birth_day, presence: true
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+  validates :email, uniqueness: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\z/i}
+end
