df262e25fda84f4e09af9eed070e727cd63b0d8d
hayashihiroaki <61njn0@gmail.com>
2020-10-01 10:47:44 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  ZENKAKU = /\A[ぁ-んァ-ン一-龥]/
+  ZENKAKU_KANA = /\A[ァ-ヶー－]+\z/
+  
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name, presence: true, format: { with: ZENKAKU }
+  validates :first_name, presence: true, format: { with: ZENKAKU }
+  validates :family_name_kana, presence: true, format: { with: ZENKAKU_KANA }
+  validates :first_name_kana, presence: true, format: { with: ZENKAKU_KANA }
   validates :birth_day, presence: true
   validates :password, format: { with: /\A[a-z0-9]+\z/i }
   validates :email, uniqueness: true
