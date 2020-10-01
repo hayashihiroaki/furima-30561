26bd31da665ba97f63830f70c86380631d3b2eff
hayashihiroaki <61njn0@gmail.com>
2020-10-01 16:46:39 +0900

    activehush、activestorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,29 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :shipping_days
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :status
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+    validates :category
+    validates :status
+    validates :delivery_charge
+    validates :shipment_source
+    validates :shipping_days
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :delivery_charge_id
+    validates :shipment_source_id
+    validates :shipping_days_id
+  end
+  
   belongs_to :user
+  has_one_attached :image
 end
