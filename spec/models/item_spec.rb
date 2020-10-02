abb6a52f10f55dece46fc188d953113223d74802
hayashihiroaki <61njn0@gmail.com>
2020-10-02 10:54:27 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,10 +33,22 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Category can't be blank")
   end
 
+  it 'カテゴリーのid情報が0の場合出品できないこと' do
+    @item.category_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category must be other than 0")
+  end
+
   it '商品の状態についての情報が必須であること' do
     @item.status_id = nil
     @item.valid?
     expect(@item.errors.full_messages).to include("Status can't be blank")
+  end
+
+  it '商品状態のid情報が0の場合出品できないこと' do
+    @item.status_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Status must be other than 0")
   end
 
   it '配送料の負担についての情報が必須であること' do
@@ -45,16 +57,34 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
   end
 
+  it '配送料の負担のid情報が0の場合出品できないこと' do
+    @item.delivery_charge_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Delivery charge must be other than 0")
+  end
+
   it '発送元の地域についての情報が必須であること' do
     @item.shipment_source_id = nil
     @item.valid?
     expect(@item.errors.full_messages).to include("Shipment source can't be blank")
   end
 
+  it '発送元の地域のid情報が0の場合出品できないこと' do
+    @item.shipment_source_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Shipment source must be other than 0")
+  end
+
   it '発送までの日数についての情報が必須であること' do
     @item.shipping_days_id = nil
     @item.valid?
     expect(@item.errors.full_messages).to include("Shipping days can't be blank")
+  end
+
+  it '発送までの日数のid情報が0の場合出品できないこと' do
+    @item.shipping_days_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Shipping days must be other than 0")
   end
 
   it '価格についての情報が必須であること' do
