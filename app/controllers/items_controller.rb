d5340269449a0c45fc38931a6f4e08b6877a4e20
hayashihiroaki <61njn0@gmail.com>
2020-10-03 15:48:54 +0900

    before_action set_item実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -36,8 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 end
@@ -52,4 +49,8 @@ def move_to_index
   @item = Item.find(params[:id])
 
   redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
