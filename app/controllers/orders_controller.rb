af0acefd5a1cd5f33c1de4fd9c61ecec7340ebb7
hayashihiroaki <61njn0@gmail.com>
2020-10-05 16:27:11 +0900

    クレジット機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
   def index
     @order = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
   end
 
   def create
-    @order = OrderAddress.new(order_params)
-    if @order.save
+    @order = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
       redirect_to root_path
     else
       render :index
@@ -14,6 +18,15 @@ class OrdersController < ApplicationController
 
   private 
   def order_params
-    params.permit(:prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+    params.permit(:prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_5fc9024271b2cbbe73450c73"  
+    Payjp::Charge.create(
+      amount: @order.price, 
+      card: order_params[:token],    
+      currency: 'jpy'    
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,14 +1,15 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number,:item_id,:user_id
+  attr_accessor :prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number,:item_id,:user_id, :token
 
   with_options presence: true do
-    validates :prefectures
+    validates :prefectures_id
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :city
     validates :house_number   
     validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :token
 
     with_options numericality: { other_than: 0 } do
       validates :prefectures_id
@@ -16,7 +17,6 @@ class OrderAddress
   end
 
   def save
-    user = User.create
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(prefectures_id: prefectures_id, postal_code: postal_code, city: city,house_number: house_number,building_name: building_name,phone_number: phone_number,order_id: order.id)
   end
