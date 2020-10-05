d7a65ccb3d82292a40a01414446fa4843e3289d2
hayashihiroaki <61njn0@gmail.com>
2020-10-05 12:21:41 +0900

    送信したデータを複数のテーブルへ保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 class OrdersController < ApplicationController
   def index
-   
+    @order = Item.find(params[:item_id])
   end
-  
+
+  def create
+    @order = OrderAddress.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private 
+  def order_params
+    params.permit(:prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,23 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :prefectures_id, :postal_code, :city, :house_number, :building_name, :phone_number,:item_id,:user_id
+
+  with_options presence: true do
+    validates :prefectures
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :city
+    validates :house_number   
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+
+    with_options numericality: { other_than: 0 } do
+      validates :prefectures_id
+    end
+  end
+
+  def save
+    user = User.create
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(prefectures_id: prefectures_id, postal_code: postal_code, city: city,house_number: house_number,building_name: building_name,phone_number: phone_number,order_id: order.id)
+  end
+end
